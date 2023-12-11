ede47cebfed7a6736b4116f7404f4a0ed0ca3724
João Vitor Bessa Lacerda <80555195+jvjzn@users.noreply.github.com>
2023-12-11 03:30:09 -0300

    correção

--- codigo/DistribuicaoRotas.java
+++ codigo/DistribuicaoRotas.java
@@ -8,28 +8,24 @@ public class DistribuicaoRotas {
 
     // Método principal para distribuir rotas entre caminhões
     public static List<Caminhao> distribuirRotas(List<Integer> rotas, int numCaminhoes) {
-        
-        // Criar uma cópia da lista de rotas
-        List<Integer> rotasMutavel = new ArrayList<>(rotas);
 
-        // Ordenar as rotas em ordem decrescente
-        rotasMutavel.sort(Collections.reverseOrder());
+        rotas.sort(Collections.reverseOrder());
 
         // Inicializar lista de caminhões
         List<Caminhao> caminhoes = new ArrayList<>();
-        for (int i = 0; i < numCaminhoes; i++) {
-            caminhoes.add(new Caminhao(i + 1));
+        for (int i = 1; i <= numCaminhoes; i++) {
+            caminhoes.add(new Caminhao(i));
         }
 
         // Distribuir as rotas entre os caminhões
-        distribuirRecursivo(rotasMutavel, caminhoes, 0);
+        distribuirRecursivo(rotas, caminhoes, 0);
 
         return caminhoes;
     }
 
     // Função recursiva para distribuir as rotas entre os caminhões
     private static void distribuirRecursivo(List<Integer> rotas, List<Caminhao> caminhoes, int caminhaoAtual) {
-        
+
         if (rotas.isEmpty()) {
             // Todas as rotas foram distribuídas
             return;
@@ -47,14 +43,14 @@ public class DistribuicaoRotas {
 
     // Main
     public static void main(String[] args) {
-        
+
         // Parâmetros para gerar as rotas
-        int qntRotas = 10;
-        int qntConjuntos = 5;
+        int qntRotas = 6;
+        int qntConjuntos = 1;
         double dispersao = 0.5;
         int numCaminhoes = 3;
 
-        // 
+        //
         List<List<Integer>> conjuntosDeTeste = new ArrayList<>();
 
         // Converte List<int[]> para List<List<Integer>>
@@ -67,7 +63,7 @@ public class DistribuicaoRotas {
         }
 
         // Distribuir as rotas
-        for(int i = 0; i < conjuntosDeTeste.size(); i++){
+        for (int i = 0; i < conjuntosDeTeste.size(); i++) {
 
             // Iniciar contagem do tempo
             long inicio = System.nanoTime();
@@ -82,11 +78,10 @@ public class DistribuicaoRotas {
             long tempoTotal = (fim - inicio);
 
             // Imprimir resultado
-            imprimirResultado(distribuicao,tempoTotal);
+            imprimirResultado(distribuicao, tempoTotal);
         }
-     
+
     }
-    
 
     // Método para imprimir o resultado da distribuição e o tempo de execução
     private static void imprimirResultado(List<Caminhao> caminhoes, long tempoTotal) {
@@ -95,7 +90,7 @@ public class DistribuicaoRotas {
             System.out.print("Rotas do caminhão " + c.getId() + " : ");
             c.imprimirRotas();
             System.out.println(" ");
-            
+
         }
         System.out.println("Tempo de execução: " + tempoTotal + "ms");
         System.out.println("-------------------------------------------");
