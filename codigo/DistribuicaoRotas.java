15feb8a5ef4542215d5dbc24d5f3d28432ca371b
João Vitor Bessa Lacerda <80555195+jvjzn@users.noreply.github.com>
2023-12-11 15:53:55 -0300

    correção

--- codigo/DistribuicaoRotas.java
+++ codigo/DistribuicaoRotas.java
@@ -1,89 +1,78 @@
 package codigo;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 public class DistribuicaoRotas {
 
-    // Método principal para distribuir rotas entre caminhões
-    public static List<Caminhao> distribuirRotas(List<Integer> rotas, int numCaminhoes) {
-
-        rotas.sort(Collections.reverseOrder());
-
-        // Inicializar lista de caminhões
-        List<Caminhao> caminhoes = new ArrayList<>();
-        for (int i = 1; i <= numCaminhoes; i++) {
-            caminhoes.add(new Caminhao(i));
-        }
-
-        // Distribuir as rotas entre os caminhões
-        distribuirRecursivo(rotas, caminhoes, 0);
-
-        return caminhoes;
-    }
-
-    // Função recursiva para distribuir as rotas entre os caminhões
-    private static void distribuirRecursivo(List<Integer> rotas, List<Caminhao> caminhoes, int caminhaoAtual) {
-
-        if (rotas.isEmpty()) {
-            // Todas as rotas foram distribuídas
-            return;
-        }
-
-        // Adicionar rota ao caminhão atual
-        caminhoes.get(caminhaoAtual).addRota(rotas.remove(0));
-
-        // Atualizar o índice do próximo caminhão
-        caminhaoAtual = (caminhaoAtual + 1) % caminhoes.size();
-
-        // Chamada recursiva para o restante das rotas
-        distribuirRecursivo(rotas, caminhoes, caminhaoAtual);
-    }
-
-    // Main
     public static void main(String[] args) {
 
-        // Parâmetros para gerar as rotas
         int qntRotas = 6;
-        int qntConjuntos = 1;
+        int qntConjuntos = 3;
         double dispersao = 0.5;
         int numCaminhoes = 3;
 
-        //
-        List<List<Integer>> conjuntosDeTeste = new ArrayList<>();
+        List<int[]> conjuntosDeTeste  = GeradorDeProblemas.geracaoDeRotas(qntRotas, qntConjuntos, dispersao);
 
-        // Converte List<int[]> para List<List<Integer>>
-        for (int[] conjunto : GeradorDeProblemas.geracaoDeRotas(qntRotas, qntConjuntos, dispersao)) {
-            List<Integer> rota = new ArrayList<>();
-            for (int i : conjunto) {
-                rota.add(i);
-            }
-            conjuntosDeTeste.add(rota);
-        }
-
-        // Distribuir as rotas
         for (int i = 0; i < conjuntosDeTeste.size(); i++) {
-
-            // Iniciar contagem do tempo
+           
             long inicio = System.nanoTime();
 
             List<Caminhao> distribuicao = distribuirRotas(conjuntosDeTeste.get(i), numCaminhoes);
-            // Imprimir resultado
-
-            // Finalizar contagem do tempo
+                       
             long fim = System.nanoTime();
-
-            // Calcular tempo total
+           
             long tempoTotal = (fim - inicio);
-
-            // Imprimir resultado
+           
             imprimirResultado(distribuicao, tempoTotal);
         }
-
     }
 
-    // Método para imprimir o resultado da distribuição e o tempo de execução
+
+    public static List<Caminhao> distribuirRotas(int[] conjuntoRotas, int caminhoes) {
+               
+        Arrays.sort(conjuntoRotas);
+              
+        inverterArray(conjuntoRotas);
+
+        List<Caminhao> caminhoesList = new ArrayList<>();
+        for (int i = 0; i < caminhoes; i++) {
+            caminhoesList.add(new Caminhao(i + 1));
+        }
+
+        int meio = conjuntoRotas.length / 2;
+        int[] subconjunto1 = Arrays.copyOfRange(conjuntoRotas, 0, meio);
+        int[] subconjunto2 = Arrays.copyOfRange(conjuntoRotas, meio, conjuntoRotas.length);
+
+        for (int i = 0; i < subconjunto1.length; i++) {
+            int indiceCaminhao = i % caminhoes;
+            caminhoesList.get(indiceCaminhao).addRota(subconjunto1[i]);
+        }
+
+        for (int i = 0; i < subconjunto2.length; i++) {
+            int indiceCaminhao = i % caminhoes;
+            caminhoesList.get(indiceCaminhao).addRota(subconjunto2[i]);
+        }
+
+        return caminhoesList;
+    }
+
+    // Método para inverter a ordem do array
+    private static void inverterArray(int[] array) {
+        int inicio = 0;
+        int fim = array.length - 1;
+
+        while (inicio < fim) {
+            int temp = array[inicio];
+            array[inicio] = array[fim];
+            array[fim] = temp;
+
+            inicio++;
+            fim--;
+        }
+    }
+
     private static void imprimirResultado(List<Caminhao> caminhoes, long tempoTotal) {
         for (Caminhao c : caminhoes) {
             System.out.println("Soma das rotas do caminhão " + c.getId() + ": " + c.somarKMcaminhao() + "km");
@@ -95,4 +84,5 @@ public class DistribuicaoRotas {
         System.out.println("Tempo de execução: " + tempoTotal + "ms");
         System.out.println("-------------------------------------------");
     }
+    
 }
